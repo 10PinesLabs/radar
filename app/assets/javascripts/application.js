fe4748aad5be18bf66e40917bc3fab514e453306
Lucas David Traverso <lucas6246@gmail.com>
2016-10-19 20:02:06 -0300

    Bare bones of a working angular thingy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require angular
 //= require_tree .
+
+angular.module('ruben-radar', [])
+    .controller('MainController', function($scope) {
+        $scope.content = 'bleh';
+    })
+;
