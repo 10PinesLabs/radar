1dd9347b95eaea02e98026cca98fab3aeb420383
gcrespi <crespi.gustavo@gmail.com>
2016-10-20 10:41:12 -0300

    A Radar cannot be created without axes

--- app/models/radar.rb
+++ app/models/radar.rb
@@ -1,5 +1,10 @@
 class Radar < ActiveRecord::Base
+
+  ERROR_MESSAGE_FOR_NO_AXES = 'The radar must have at least one axis'
+
   has_and_belongs_to_many :axes
+
+  validates :axes, presence: { message: ERROR_MESSAGE_FOR_NO_AXES }
 
   delegate :empty?, to: :axes
 
