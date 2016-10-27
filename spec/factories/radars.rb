8ee3f240cdf922e146b47d5959b2ac2185ea76c8
Lucas David Traverso <lucas6246@gmail.com>
2016-10-27 14:54:18 -0300

    Hardcoded the default amount of axis

--- spec/factories/radars.rb
+++ spec/factories/radars.rb
@@ -2,12 +2,8 @@ FactoryGirl.define do
   factory :radar do
     active true
 
-    transient do
-      axes_count 5
-    end
-
     after(:build) do |radar, evaluator|
-      radar.axes.push build_list(:axis, evaluator.axes_count)
+      radar.axes.push build_list(:axis, 3)
     end
   end
 end
