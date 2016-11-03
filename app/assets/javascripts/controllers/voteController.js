4fc8b9396398a38c3137c08e85d7017da0a0f9b2
gcrespi <crespi.gustavo@gmail.com>
2016-11-03 17:29:40 -0300

    Fixed flicker + Successful vote enhacement

--- app/assets/javascripts/controllers/voteController.js
+++ app/assets/javascripts/controllers/voteController.js
@@ -1,17 +1,20 @@
 angular.module('ruben-radar')
     .controller('VoteController', function ($location, $scope, _, VoteService, radar, ngToast) {
         $scope.radar = radar;
-        $scope.vote = VoteService.newVote(radar);
+        $scope.answers = _.map(radar.axes, function (axis) {
+            return {axis: axis, points: undefined};
+        });
         $scope.options = _.range(1, 6);
 
         $scope.allAxesAnswered = function () {
-            return _.every($scope.vote.answers, function (answer) {
+            return _.every($scope.answers, function (answer) {
                 return answer.points !== undefined;
             });
         };
 
         $scope.submit = function submit() {
-            VoteService.createVote($scope.vote).then(function () {
+            var vote = VoteService.newVote(radar, $scope.answers);
+            VoteService.createVote(vote).then(function () {
                 $location.path('/successfulVote');
             }).catch(function (errors) {
                 _.forEach(errors, function(error){
