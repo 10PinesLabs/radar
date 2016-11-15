8bcb61999855432bfb7b08383eafb698c3999e04
gcrespi <crespi.gustavo@gmail.com>
2016-11-15 15:02:28 -0300

    Select to Compare View enhanced

--- app/assets/javascripts/controllers/radarsController.js
+++ app/assets/javascripts/controllers/radarsController.js
@@ -1,16 +1,33 @@
 angular.module('ruben-radar')
-    .controller('RadarsController', function ($scope, $location, _, radars) {
+    .controller('RadarsController', function ($scope, $location, _, radars, ngToast) {
         $scope.closedRadars = _.filter(radars, function (radar) {
             return !radar.active;
         });
+        $scope.closedRadars = _.sortBy($scope.closedRadars, 'created_at');
 
         $scope.beforeRadar = undefined;
         $scope.afterRadar = undefined;
 
         $scope.compareChosenRadars = function () {
-            $location.path('/radars/compare').search({
-                'beforeResult': $scope.beforeRadar.id,
-                'afterResult': $scope.afterRadar.id
-            });
+            if($scope.sameRadarSelected()) {
+                ngToast.danger('No se puede comparar contra el mismo radar');
+            } else {
+                $location.path('/radars/compare').search({
+                    'beforeResult': $scope.beforeRadar.id,
+                    'afterResult': $scope.afterRadar.id
+                });
+            }
+        };
+
+        $scope.bothRadarsSelected = function () {
+            return $scope.afterRadar !== undefined && $scope.beforeRadar !== undefined;
+        };
+
+        $scope.sameRadarSelected = function () {
+            return $scope.afterRadar === $scope.beforeRadar;
+        };
+
+        $scope.cannotCompareRadars = function () {
+            return !$scope.bothRadarsSelected();
         };
     });
