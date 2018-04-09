b124c3823af2564151c6961292eba7b27a4fbe04
Micaela <micaela.alonso@10pines.com>
2018-04-09 15:51:19 -0300

    resultsInCSV shows download button

--- app/assets/javascripts/controllers/ResultsInCSVController.js
+++ app/assets/javascripts/controllers/ResultsInCSVController.js
@@ -1,8 +1,10 @@
 angular.module('ruben-radar')
-    .controller('ResultsInCSVController', function ($scope, _, radar) {
+    .controller('ResultsInCSVController', function ($scope, $window, _, radar) {
         $scope.radar = radar;
         $scope.axes = radar.axes;
-
+        var blob = new Blob(["hola"], { type: 'text/plain' });
+            url = $window.URL || $window.webkitURL;
+        $scope.fileUrl = url.createObjectURL(blob);
 
         function answerIsOne(answer) {
             return answer.points === 1
