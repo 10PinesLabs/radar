6ceb411cb00979c4b52f1d1502e844179c01d67d
Micaela <micaela.alonso@10pines.com>
2018-04-09 13:05:22 -0300

    resultsInCVS view shows amount of people that voted each piont for each axis

--- app/assets/javascripts/controllers/ResultsInCSVController.js
+++ app/assets/javascripts/controllers/ResultsInCSVController.js
@@ -2,6 +2,48 @@ angular.module('ruben-radar')
     .controller('ResultsInCSVController', function ($scope, _, radar) {
         $scope.radar = radar;
         $scope.axes = radar.axes;
-        
+
+
+        function answerIsOne(answer) {
+            return answer.points === 1
+        }
+
+
+
+        $scope.amountOfVotesFor1 = function (axis) {
+            return "," + axis.answers.filter(answerIsOne).length;
+        }
+
+        function answerIsTwo(answer) {
+            return answer.points === 2
+        }
+
+        $scope.amountOfVotesFor2 = function (axis) {
+            return "," + axis.answers.filter(answerIsTwo).length;
+        }
+
+        function answerIsThree(answer) {
+            return answer.points === 3
+        }
+
+        $scope.amountOfVotesFor3 = function (axis) {
+            return "," + axis.answers.filter(answerIsThree).length;
+        }
+
+        function answerIsFour(answer) {
+            return answer.points === 4
+        }
+
+        $scope.amountOfVotesFor4 = function (axis) {
+            return "," + axis.answers.filter(answerIsFour).length;
+        }
+
+        function answerIsFive(answer) {
+            return answer.points === 5
+        }
+
+        $scope.amountOfVotesFor5 = function (axis) {
+            return "," + axis.answers.filter(answerIsFive).length;
+        }
 
     });
