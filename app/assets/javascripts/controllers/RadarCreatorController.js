87382bb03cdfbb70448e74f60ac05a259098d899
Joel <joel.camera@10pines.com>
2018-04-10 09:23:56 -0300

    newRadar function without $scope as parmeter.

--- app/assets/javascripts/controllers/RadarCreatorController.js
+++ app/assets/javascripts/controllers/RadarCreatorController.js
@@ -12,13 +12,13 @@ angular.module('ruben-radar')
             if($scope.description.length === 0 || $scope.axes.length === 0){
                 createNgToast('danger', 'No se puede crear un radar sin descripción ni axes.');
             } else {
-                RadarService.createRadar(newRadar($scope)).then(function () {
+                RadarService.createRadar(newRadar()).then(function () {
                     createNgToast('success', 'Se ha creado el radar con éxito');
                 })
             }
         };
 
-        var newRadar = function ($scope) {
+        var newRadar = function () {
             return {
                 description: $scope.description,
                 axes: $scope.axes
