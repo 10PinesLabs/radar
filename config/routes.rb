765a06ff5a968899b0e1bba71c4ab42b9dd5175a
Joel <joel.camera@10pines.com>
2018-04-12 11:15:01 -0300

    Delete comment in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # buscar en public, si esta lo devuelvo
   scope path: '/api' do
     resources :radars, only: %i[create show index] do
       resources :votes, only: [:create]
