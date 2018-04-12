2e0cde6245c2957af238448db5445ca2e836175d
Joel <joel.camera@10pines.com>
2018-04-12 11:04:40 -0300

    Added prefix '/api' to api routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :radars, only: %i[create show index] do
-    resources :votes, only: [:create]
-    member do
-      get :result
-      post :create
-      post :close
+  # buscar en public, si esta lo devuelvo
+  scope path: '/api' do
+    resources :radars, only: %i[create show index] do
+      resources :votes, only: [:create]
+      member do
+        get :result
+        post :create
+        post :close
+      end
     end
   end
   root to: 'application#angular'
