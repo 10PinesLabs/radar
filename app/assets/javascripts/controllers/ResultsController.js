fd7bad1debce56f606081eb5d6a96c638fdefc08
Micaela <micaela.alonso@10pines.com>
2018-04-12 13:55:06 -0300

    deletes comment

--- app/assets/javascripts/controllers/ResultsController.js
+++ app/assets/javascripts/controllers/ResultsController.js
@@ -14,28 +14,7 @@ angular.module('ruben-radar')
 
         }
 
-        // function axesRows() {
-        //     var axisRow = ""
-        //     for (var i = 0; i < $scope.axes.length; i++) {
-        //         var axis = $scope.axes[i];
-        //         axisRow = axisRow + axis.description + amountsOfVotes(axis) + "\n";
-        //
-        //     }
-        //     return axisRow
-        // }
-        //
-        // function amountsOfVotes(axis) {
-        //     var votes = ""
-        //     for (var n = 1; n <= 5; n++) {
-        //         votes = votes + amountOfPeopleThatVoted(axis.answers, n)
-        //     }
-        //     return votes;
-        // }
-        //
-        // function amountOfPeopleThatVoted(answers, n) {
-        //     var answersWithPointN =  _.filter(answers, function (answer) {return answer.points === n})
-        //     return "; " + _.size(answersWithPointN)
-        // }
+        
 
 
     });
