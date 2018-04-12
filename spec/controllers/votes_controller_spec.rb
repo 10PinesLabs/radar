bfb996784a124f029d4e3056c1d30d08515ba1a4
Joel <joel.camera@10pines.com>
2018-04-12 13:56:29 -0300

    Fixed problem with repeated names in spec and factory bot.

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe VotesController, type: :controller do
       end
 
       context 'and the answers are from different radars' do
-        let(:another_radar) { create :radar }
+        let(:another_radar) { create :different_radar }
 
         it 'should return an unprocessable entity' do
           request_to_create_vote(a_radar.id, a_radar.axes + another_radar.axes)
--- spec/factories/radars.rb
+++ spec/factories/radars.rb
@@ -4,4 +4,8 @@ FactoryBot.define do
     name 'Retiro 2015'
     description 'Retiro 2015'
   end
+
+  factory :different_radar, parent: :radar do
+    name 'Otro Retiro 2015'
+  end
 end
