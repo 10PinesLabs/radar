b356b93a57c82d6fd3c959a4c61992c7e012e0d4
Micaela <micaela.alonso@10pines.com>
2018-04-13 16:55:09 -0300

    radars are shown from newer to older

--- app/assets/javascripts/controllers/IndexController.js
+++ app/assets/javascripts/controllers/IndexController.js
@@ -1,9 +1,12 @@
 angular.module('ruben-radar')
     .controller('IndexController', function ($scope, $mdDialog, _, radars) {
-        $scope.radars = radars;
+        $scope.radars = _.reverse(radars);
         $scope.radarsDate = function (radar) {
             var date = radar.created_at
             return date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear();
+        }
+        $scope.date = function (radar) {
+            return ;
         }
 
     })
