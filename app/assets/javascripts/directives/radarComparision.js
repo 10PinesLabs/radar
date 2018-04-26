1d356d31c56962bd7f36436e67968513efc70525
Micaela <micaela.alonso@10pines.com>
2018-04-26 11:11:05 -0300

    when I click an axis legend in calls getData function in directive

--- app/assets/javascripts/directives/radarComparision.js
+++ app/assets/javascripts/directives/radarComparision.js
@@ -11,10 +11,8 @@ angular.module('ruben-radar')
 
                 Representation: '='
             },
+            dataForBarChar: {},
             link: function (scope, element) {
-                scope.getData = function(axis) {
-                    console.log("Click!")
-                }
                 var defaultConfig = {
                     steps: 5,
                     maxValue: 5,
@@ -27,6 +25,31 @@ angular.module('ruben-radar')
                 var radarSize = new Vector2D(500, 500);
                 new RadarChart(radarSize, config.steps, config.maxValue, config.Representation).draw(element[0], strategy, this);
                 console.log(this)
+
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['1', '2', '3', '4', '5'],
+                        datasets: [{
+                            label: 'apples',
+                            data: [12, 19, 3, 17, 6, 3, 7],
+                            backgroundColor: "rgba(26,129,102,0.2)",
+                            borderColor: "#3cba9f",
+                            //fill:false
+                        }, {
+                            label: 'oranges',
+                            data: [2, 29, 5, 5, 2, 3, 10],
+                            backgroundColor: "rgba(255,153,0,0.2)",
+                            borderColor: "rgba(179,11,198,1)",
+                            //fill:false
+                        }]
+                    }
+                });
+            },
+            getData: function(axis){
+                console.log(axis)
             }
+
         };
     });
