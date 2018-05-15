9eefef27fd6cef4fca1a523465e1b7cf6f2c7c88
Joel <joel.camera@10pines.com>
2018-05-15 12:12:40 -0300

    Order radars in index by create_at date.

--- app/assets/javascripts/controllers/IndexController.js
+++ app/assets/javascripts/controllers/IndexController.js
@@ -1,7 +1,10 @@
 angular.module('ruben-radar')
     .controller('IndexController', function ($scope, RadarService, _, radars) {
-        $scope.isOpen = false;
-        $scope.radars = _.reverse(radars);
+        $scope.radars = radars.sort(function(a, b) {
+            a = new Date(a.created_at);
+            b = new Date(b.created_at);
+            return a>b ? -1 : a<b ? 1 : 0;
+        });
 
         $scope.logout = function logout() {
             RadarService.signOut();
