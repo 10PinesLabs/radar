972805daa2d4a86bcf37e081cc29591ccf012bce
GianFF <gian.franco.fioriello@10pines.com>
2018-06-22 18:14:29 -0300

    Agrego Login Logout button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  LOGIN = 'Login';
+  LOGOUT = 'Logout';
+
+  button_text: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.button_text = this.LOGIN;
   }
 
+  onClickLoginLogoutButton() {
+    const userIsLoggedIn = this.button_text === this.LOGIN;
+
+    if (userIsLoggedIn) {
+      this.button_text = this.LOGOUT;
+    } else {
+      this.button_text = this.LOGIN;
+    }
+  }
 }
