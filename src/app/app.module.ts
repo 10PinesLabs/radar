f38e1ce66bd33b58e09dd2fd92c09625dbc60d47
GianFF <gian.franco.fioriello@10pines.com>
2018-08-01 18:14:31 -0300

    Agrego AxisComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RadarVoteComponent } from './radar-vote/radar-vote.component';
+import { AxisComponent } from './axis/axis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RadarVoteComponent
+    RadarVoteComponent,
+    AxisComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/axis/axis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-axis',
+  templateUrl: './axis.component.html',
+  styleUrls: ['./axis.component.scss']
+})
+export class AxisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
