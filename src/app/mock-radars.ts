d90b6978ff83d5c84089ca740363b2b51a20306e
GianFF <gian.franco.fioriello@10pines.com>
2018-08-02 22:01:07 -0300

    Extraigo creación de objetos mock fuera del servicio fake

---
+++ src/app/mock-radars.ts
@@ -0,0 +1,14 @@
+import {Radar} from '../model/radar';
+import {Axis} from '../model/axis';
+
+export const AXES: Axis[] = [
+  new Axis('Calidad técnica', 'La calidad técnica representa el eje...'),
+  new Axis('Calidad humana', 'La calidad humana representa el eje...'),
+  new Axis('Ambiente laboral', 'El ambiente laboral representa el eje...')
+];
+
+export const RADARS: Radar[] = [
+  new Radar('Radar 2016', 'Radar utilizado en el Retiro Estrategico 10Pines 2018', AXES, 1),
+  new Radar('Radar 2017', 'Radar utilizado en el Retiro Estrategico 10Pines 2018', AXES, 2),
+  new Radar('Radar 2018', 'Radar utilizado en el Retiro Estrategico 10Pines 2018', AXES, 3)
+];
--- src/app/radar.service.ts
+++ src/app/radar.service.ts
@@ -1,32 +1,15 @@
 import { Injectable } from '@angular/core';
 import {Radar} from '../model/radar';
-import {Axis} from '../model/axis';
+import {RADARS} from './mock-radars';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RadarService {
-  calidadTecnica: Axis;
-  calidadHumana: Axis;
-  ambienteLaboral: Axis;
-  axes: Array<Axis>;
-  radar2016: Radar;
-  radar2017: Radar;
-  radar2018: Radar;
-
   radares: Radar[];
 
   constructor() {
-    this.calidadTecnica = new Axis('Calidad técnica', 'La calidad técnica representa el eje...');
-    this.calidadHumana = new Axis('Calidad humana', 'La calidad humana representa el eje...');
-    this.ambienteLaboral = new Axis('Ambiente laboral', 'El ambiente laboral representa el eje...');
-    this.axes = [this.calidadTecnica, this.calidadHumana, this.ambienteLaboral];
-
-    this.radar2016 = new Radar('Radar 2016', 'Radar utilizado en el Retiro Estrategico 10Pines 2018', this.axes, 1);
-    this.radar2017 = new Radar('Radar 2017', 'Radar utilizado en el Retiro Estrategico 10Pines 2018', this.axes, 2);
-    this.radar2018 = new Radar('Radar 2018', 'Radar utilizado en el Retiro Estrategico 10Pines 2018', this.axes, 3);
-
-    this.radares = [this.radar2016, this.radar2017, this.radar2018];
+    this.radares = RADARS;
   }
 
   radar(radarId: any): Radar {
