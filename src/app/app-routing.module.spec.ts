2c29234a5076a664bcc9dfe435dfd26042d7dc74
GianFF <gian.franco.fioriello@10pines.com>
2018-08-14 20:13:01 -0300

    elimino tests de componentes que no testean nada

--- src/app/app-routing.module.spec.ts
+++
@@ -1,9 +0,0 @@
-import { AppRoutingModule } from './app-routing.module';
-
-describe('AppRoutingModule', () => {
-  let appRoutingModule: AppRoutingModule;
-
-  beforeEach(() => {
-    appRoutingModule = new AppRoutingModule();
-  });
-});
