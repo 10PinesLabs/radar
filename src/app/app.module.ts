b36d2327a5c3d096e534134885788919d905cafb
Joel <joel.camera@10pines.com>
2018-12-04 16:53:29 -0300

    Added view when voted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { RadarVoteComponent } from './radar-vote/radar-vote.component';
 import { AxisComponent } from './axis/axis.component';
 import { environment} from '../environments/environment';
+import { VotingRadarComponent } from './radar-vote/voting-radar/voting-radar.component';
+import { VotedRadarComponent } from './radar-vote/voted-radar/voted-radar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RadarVoteComponent,
-    AxisComponent
+    AxisComponent,
+    VotingRadarComponent,
+    VotedRadarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/radar-vote/voted-radar/voted-radar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voted-radar',
+  templateUrl: './voted-radar.component.html',
+  styleUrls: ['./voted-radar.component.scss']
+})
+export class VotedRadarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
