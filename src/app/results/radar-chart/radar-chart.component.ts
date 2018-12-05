9975e69ea68d0fdece17125823f8fb973bfbf791
Joel <joel.camera@10pines.com>
2018-12-05 16:17:07 -0300

    WIP: Sincronize radar chart with radar

--- src/app/results/radar-chart/radar-chart.component.ts
+++ src/app/results/radar-chart/radar-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { Radar } from 'src/model/radar';
+import { Axis } from 'src/model/axis';
 
 @Component({
   selector: 'app-radar-chart',
@@ -10,10 +11,10 @@ import { Radar } from 'src/model/radar';
 export class RadarChartComponent implements AfterViewInit {
 
   @ViewChild('radarChartId') canvasRef: ElementRef;
-  // @Input() radar: Radar;
+  @Input() radar: Radar;
   radarChart = [];
 
-  constructor(private elementRef: ElementRef) { }
+  constructor() { }
 
   ngAfterViewInit() {
     setTimeout(() => {
@@ -23,34 +24,76 @@ export class RadarChartComponent implements AfterViewInit {
 
   createRadarChart() {
     const ctx = this.canvasRef.nativeElement.getContext('2d');
-    const data = {
-      labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
+    const radarData = this.parseRadarData();
+    const radarOptions = this.parseRadarOptions();
+
+    this.radarChart = new Chart(ctx, {
+      type: 'radar',
+      data: radarData,
+      options: radarOptions,
+    });
+  }
+
+
+  private parseRadarData() {
+    const radarLabel = this.radar.title;
+    const radarBackgroundColor = 'rgba(157, 217, 191, 0.6)';
+    const radarBorderColor = 'rgba(25, 179, 112, 1)';
+
+    const axisValues = this.radar.axisValues();
+    const axisLabels = [];
+    const axisMedians = [];
+    axisValues.forEach(axisValue => {
+      const axisTitle = axisValue[0].title;
+      const median = this.medianFor(axisValue[1]);
+
+      axisLabels.push(axisTitle);
+      axisMedians.push(median);
+    });
+
+    return {
+      labels: axisLabels,
       datasets: [{
-        label: 'Student A',
-        backgroundColor: 'rgba(157, 217, 191, 0.6)',
-        borderColor: 'rgba(25, 179, 112, 1)',
+        label: radarLabel,
+        backgroundColor: radarBackgroundColor,
+        borderColor: radarBorderColor,
         fill: true,
         radius: 6,
         pointRadius: 6,
         pointBorderWidth: 3,
-        pointBackgroundColor: 'rgba(157, 217, 191, 0.6)',
-        pointBorderColor: 'rgba(25, 179, 112, 1)',
+        pointBackgroundColor: radarBackgroundColor,
+        pointBorderColor: radarBorderColor,
         pointHoverRadius: 10,
-        data: [20, 10, 4, 2],
+        data: axisMedians,
       }]
     };
+  }
 
-    const options = {
-      startAngle: -Math.PI / 4,
+  private parseRadarOptions() {
+    return {
       legend: {
-        display: false
+        display: true,
       },
     };
+  }
 
-    this.radarChart = new Chart(ctx, {
-      type: 'radar',
-      data: data,
-      options: options,
-    });
+  private axisValuesObjToArray(axisValues) {
+    const axisValuesArray = [];
+    [1, 2, 3, 4, 5].forEach(field => axisValuesArray.push(axisValues[field]) );
+    return axisValuesArray;
+  }
+
+  private medianFor(axisValues) {
+    const values = this.axisValuesObjToArray(axisValues);
+    let median;
+
+    if (values.length === 0) {
+      median =  0;
+    } else {
+      const m = values.length / 2;
+      median = values.length % 2 === 0 ? (values[m] + values[m - 1]) / 2 : values[m];
+    }
+
+    return median;
   }
 }
