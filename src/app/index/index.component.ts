54c98e3a79d8edbc479451bbd83ba074e3ba8703
Joel <joel.camera@10pines.com>
2018-12-28 16:11:24 -0300

    Logica de botones para cada card

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { RadarService } from 'src/services/radar.service';
 import { Radar } from 'src/model/radar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -11,10 +12,27 @@ export class IndexComponent implements OnInit {
 
   radars: Radar[];
 
-  constructor(@Inject('RadarService') private radarService: RadarService) { }
+  constructor(@Inject('RadarService') private radarService: RadarService, private router: Router) { }
 
   ngOnInit() {
     this.radarService.radars().subscribe(radars => this.radars = radars);
   }
 
+  redirectToRadar(radarId: any) {
+    this.router.navigate(['/radar/' + radarId + '/results']);
+  }
+
+  copyVoteRadarLink(radarId): any {
+    const urlToCopy = window.location.host + '/radar/' + radarId + '/vote';
+    const elem = document.createElement('textarea');
+    elem.value = urlToCopy;
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+  }
+
+  closeRadar(radarId: any) {
+    this.radarService.close(radarId);
+  }
 }
