eb4b371280b7c67de331ee3256688a002fe20741
Joel <joel.camera@10pines.com>
2019-02-01 18:04:09 -0300

    WIP

--- src/app/select-to-compare/select-to-compare.component.ts
+++ src/app/select-to-compare/select-to-compare.component.ts
@@ -11,12 +11,18 @@ export class SelectToCompareComponent implements OnInit {
 
   title: String = 'Comparar Radares';
   radars: Radar[];
+  firstRadar: Radar;
+  secondRadar: Radar;
 
   constructor(@Inject('RadarService') private radarService: RadarService) { }
 
   ngOnInit() {
-    this.radarService.radars().subscribe(radars => this.radars = radars);
-    this.radars = this.radars.sort((r1, r2) => r2.id - r1.id); // mayor id a menor id
+    this.radarService.radars().subscribe(radars => {
+      this.radars = radars;
+      this.radars = this.radars.sort((r1, r2) => r2.id - r1.id); // mayor id a menor id
+      this.firstRadar = this.radars[0];
+      this.secondRadar = this.radars[0];
+    });
   }
 
 }
