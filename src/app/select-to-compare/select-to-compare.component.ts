a8918ca44a2db72307d3ba4ecec48936e3354f43
Joel <joel.camera@10pines.com>
2019-02-02 19:06:45 -0300

    Agregado boton para volver al index en select-to-compare

--- src/app/select-to-compare/select-to-compare.component.ts
+++ src/app/select-to-compare/select-to-compare.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Radar } from 'src/model/radar';
 import { RadarService } from 'src/services/radar.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-select-to-compare',
@@ -14,7 +15,7 @@ export class SelectToCompareComponent implements OnInit {
   firstRadar: Radar;
   secondRadar: Radar;
 
-  constructor(@Inject('RadarService') private radarService: RadarService) { }
+  constructor(@Inject('RadarService') private radarService: RadarService, private router: Router) { }
 
   ngOnInit() {
     this.radarService.radars().subscribe(radars => {
@@ -23,5 +24,9 @@ export class SelectToCompareComponent implements OnInit {
       this.firstRadar = this.radars[0];
       this.secondRadar = this.radars[0];
     });
+  }
+
+  backToIndex() {
+    this.router.navigate(['/radars']);
   }
 }
