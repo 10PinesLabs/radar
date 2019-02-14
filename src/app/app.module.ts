57e1b69e30168df7ee09cfc478b1c7b88ec52884
Joel <joel.camera@10pines.com>
2019-02-13 23:46:33 -0300

    Paso componentes de graficos a carpeta '/chart-components'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,6 @@ import { VotingRadarComponent } from './radar-vote/voting-radar/voting-radar.com
 import { VotedRadarComponent } from './radar-vote/voted-radar/voted-radar.component';
 import { ResultsComponent } from './results/results.component';
 import { CardContainerComponent } from './card-container/card-container.component';
-import { AxisBarChartComponent } from './results/axis-bar-chart/axis-bar-chart.component';
-import { AxisTableValuesComponent } from './results/axis-table-values/axis-table-values.component';
-import { RadarChartComponent } from './results/radar-chart/radar-chart.component';
 import { IndexComponent } from './index/index.component';
 import { RadarCardComponent } from './index/radar-card/radar-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,6 +20,9 @@ import { RadarFormComponent } from './create-radar/radar-form/radar-form.compone
 import { AxesFormComponent } from './create-radar/axes-form/axes-form.component';
 import { SelectToCompareComponent } from './select-to-compare/select-to-compare.component';
 import { CompareRadarsComponent } from './compare-radars/compare-radars.component';
+import { AxisBarChartComponent } from './chart-components/axis-bar-chart/axis-bar-chart.component';
+import { AxisTableValuesComponent } from './chart-components/axis-table-values/axis-table-values.component';
+import { RadarChartComponent } from './chart-components/radar-chart/radar-chart.component';
 
 @NgModule({
   declarations: [
