0461996941f8832a4269595715a210ff0d7b3d7d
gianff <gf.fioriello@gmail.com>
2019-02-22 19:24:33 -0300

    Modifico componente Token para que redirija a la vista de radares

--- src/app/token/token.component.ts
+++ src/app/token/token.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
 
 @Component({
@@ -9,12 +9,14 @@ import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
 })
 export class TokenComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute,
+  constructor(private router: Router,
+              private route: ActivatedRoute,
               @Inject(LOCAL_STORAGE) private storage: WebStorageService) { }
 
   ngOnInit() {
     const token = this.route.snapshot.paramMap.get('token');
     this.storage.set('token', token);
+    this.router.navigate(['/radars']);
   }
 
 }
