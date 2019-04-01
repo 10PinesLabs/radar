3a69f8d000b556468d2f18fffc8d7b21ecdf1779
Lucas David Traverso <lucas6246@gmail.com>
2019-04-01 19:18:06 -0300

    Changes for creating staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://ruben-radar.herokuapp.com',
+  apiURL: 'https://ruben-radar-stg.herokuapp.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@
 
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:3000'
+  apiURL: 'http://localhost:3000',
 };
