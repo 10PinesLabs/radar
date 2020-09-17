f6a7db8bc467504bfd5bc479701b9c56e63c21e7
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-09-17 13:09:03 -0300

    ahora se pueden agregar/quitar logins de manera sensilla a travez de variables de ambiente

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -8,11 +8,23 @@ import { environment } from 'src/environments/environment';
 })
 export class SignInComponent implements OnInit {
 
+  providers = []
+  
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.providers = environment.logins
+  }
 
   apiURL() {
     return environment.apiURL;
+  }
+
+  loginWith(providerName) {
+    return () => window.location.href = this.providerUrl(providerName);
+  }
+
+  providerUrl(providerName){
+    return `${ this.apiURL() }/auth/${providerName}/redirect`
   }
 }
