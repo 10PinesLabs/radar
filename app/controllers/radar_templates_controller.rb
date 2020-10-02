adb4606afc6b18374eae613f730775b601549af4
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-10-02 12:07:13 -0300

    rename with_radar

--- app/controllers/radar_templates_controller.rb
+++ app/controllers/radar_templates_controller.rb
@@ -11,8 +11,8 @@ class RadarTemplatesController < ApplicationController
   end
 
   def show
-    template_id = params.require(:id)
-    with_radar template_id do |template|
+    radar_template_id = params.require(:id)
+    if_radar_present radar_template_id do |template|
       render json: template, status: :ok
     end
   end
@@ -23,9 +23,9 @@ class RadarTemplatesController < ApplicationController
 
   def share
     shared_user = User.find(params.require(:user_id))
-    template_id = params.require(:radar_template_id)
+    radar_template_id = params.require(:radar_template_id)
 
-    with_radar template_id do |template|
+    if_radar_present radar_template_id do |template|
       begin
         template.agregar_usuario(@logged_user, shared_user)
         render status: :ok, json: "El radar se compartio satisfactoriamente"
@@ -42,7 +42,7 @@ class RadarTemplatesController < ApplicationController
     Axis.new(name: axis.require(:name), description: axis['description'])
   end
 
-  def with_radar template_id
+  def if_radar_present template_id
     radar_template = RadarTemplate.find(template_id)
     render_not_found unless radar_template.is_known_by? @logged_user
     yield radar_template
